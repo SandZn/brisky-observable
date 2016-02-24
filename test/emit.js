9208a8acb6d0b602939c6aa253bd0d11bfe8c866
Jim de Beer <jim@vigour.io>
2016-02-24 09:52:56 +0100

    added tests, fixed empty listeners

--- test/emit.js
+++
@@ -1,9 +0,0 @@
-console.log('lullllz')
-var Observable = require('../')
-var a = new Observable('a')
-a.key = 'hello'
-a.on('data', function () {
-  console.error('yo yo yo', this.path)
-})
-console.log(a.val)
-a.val = 'xxxx'
