3ee30c6a4e5cc8c5fcb51af68bac4088bdcec986
Jim de Beer <jim@vigour.io>
2016-03-01 15:29:28 +0100

    added context test, removed unnsececary comments

--- test/context.js
+++ test/context.js
@@ -1,6 +1,46 @@
 'use strict'
 var Observable = require('../')
 var test = require('tape')
-var Base = require('vigour-base')
 
+test('child constructors and context', function (t) {
+  t.plan(1)
+  var Obs = new Observable({
+    define: {
+      handleContextChild (data, event, context) {
+        this.clearContext()
+        this.emit('data', data, event)
+      }
+    },
+    on: {
+      data () {}
+    },
+    Child: 'Constructor'
+  }).Constructor
 
+  var keys = {}
+
+  var injectable = {
+    api: {
+      type: 'observable',
+      components: {
+        api: {
+          on: {
+            data: {
+              api () {
+                let key = this.origin.key
+                !keys[key] ? keys[key] = 1 : keys[key]++
+              }
+            }
+          }
+        }
+      },
+      Child: { type: 'api' },
+      language: true
+    }
+  }
+  Obs.prototype.inject(injectable)
+  var o = new Obs()
+  var ref = new Obs({ key: 'ref' })
+  o.api.language.val = ref
+  t.equal(keys.ref, 1)
+})
