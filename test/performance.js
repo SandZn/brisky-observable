5ba79f00c5604509efe56b2218e6bffa9d984278
Jim de Beer <jim@vigour.io>
2016-03-22 14:44:34 +0100

    refactor events

--- test/performance.js
+++ test/performance.js
@@ -1,24 +1,15 @@
 'use strict'
 const Observable = require('../')
 const Base = require('vigour-base')
-const Event = require('vigour-event')
 const Observ = require('observ')
 // const ObservStruct = require('observ-struct')
 const test = require('tape')
 const perf = require('vigour-performance').run
 
 var observResult
+var amount = 1e6
 
-test('event', function (t) {
-  // optmize this as well but later
-  perf(() => {
-    var ev = new Event() // this is insanity!
-    ev.trigger()
-  }, (ms) => {
-    console.log('vigour-event', ms + 'ms')
-    t.end()
-  }, 1e6)
-})
+amount = 10
 
 test('base', function (t) {
   const o = new Base()
@@ -27,26 +18,23 @@ test('base', function (t) {
   perf(() => o.set(++cnt), (ms) => {
     console.log('vigour-base', ms + 'ms')
     t.end()
-  }, 1e6)
+  }, amount)
 })
 
 test('observ', function (t) {
   const o = Observ(0)
   var callCount = 0
-  var callCount2 = 0
   var cnt = 0
   o(() => ++callCount)
-  o(() => ++callCount2)
+  o(() => ++callCount)
   perf(() => o.set(++cnt), (ms) => {
-    console.log('observ', ms + 'ms', callCount, callCount2)
+    console.log('observ', ms + 'ms', callCount)
     observResult = ms
     t.end()
-  }, 1e6)
+  }, amount)
 })
 
 test('base-observer', function (t) {
-  // like observ-struct but on a base (and faster then observ)
-  // make this faster as well
   const _set = Base.prototype.set
   const o = new Base({
     define: {
@@ -69,24 +57,24 @@ test('base-observer', function (t) {
   })
   var cnt = 0
   var callCount = 0
-  var callCount2 = 0
   o.on(() => ++callCount)
-  o.on(() => ++callCount2)
+  o.on(() => ++callCount)
   perf(() => o.set(++cnt), (ms) => {
     console.log('vigour-base-observer', ms + 'ms', callCount, Math.round((ms / observResult) * 100) + '%')
     t.end()
-  }, 1e6)
+  }, amount)
 })
 
 test('observable', function (t) {
   const o = new Observable(0)
   var callCount = 0
-  var callCount2 = 0
   var cnt = 0
+  console.log(o)
+  o.set('???????????????')
   o.on(() => ++callCount)
-  o.on(() => ++callCount2)
+  o.on(() => ++callCount)
   perf(() => o.set(++cnt), (ms) => {
     console.log('vigour-observable', ms + 'ms', callCount, Math.round((ms / observResult) * 100) + '%')
     t.end()
-  }, 1e6)
+  }, amount)
 })
