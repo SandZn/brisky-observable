3ab2b339744c7f94c467dc674517c89aac743665
Jim de Beer <jim@vigour.io>
2016-03-23 23:30:37 +0100

    removing unnsecary files

--- test/performance.js
+++ test/performance.js
@@ -7,17 +7,17 @@ const test = require('tape')
 const perf = require('vigour-performance').run
 
 var observResult
-var amount = 1e5
+var amount = 3e6
 
-test('base', function (t) {
-  const o = new Base()
-  var cnt = 0
-  // optmize this as well but later
-  perf(() => o.set(++cnt), (ms) => {
-    console.log('vigour-base', ms + 'ms')
-    t.end()
-  }, amount)
-})
+// test('base', function (t) {
+//   const o = new Base()
+//   var cnt = 0
+//   // optmize this as well but later
+//   perf(() => o.set(++cnt), (ms) => {
+//     console.log('vigour-base', ms + 'ms')
+//     t.end()
+//   }, amount)
+// })
 
 test('observ', function (t) {
   const o = Observ(0)
@@ -32,36 +32,36 @@ test('observ', function (t) {
   }, amount)
 })
 
-test('base-observer', function (t) {
-  const _set = Base.prototype.set
-  const o = new Base({
-    define: {
-      set (val) {
-        var on = this._on
-        if (on) {
-          for (var i = 0, len = on.length; i < len; i++) {
-            on[i](val)
-          }
-        }
-        return _set.apply(this, arguments)
-      },
-      on (fn) {
-        if (!this._on) {
-          this._on = []
-        }
-        this._on.push(fn)
-      }
-    }
-  })
-  var cnt = 0
-  var callCount = 0
-  o.on(() => ++callCount)
-  o.on(() => ++callCount)
-  perf(() => o.set(++cnt), (ms) => {
-    console.log('vigour-base-observer', ms + 'ms', callCount, Math.round((ms / observResult) * 100) + '%')
-    t.end()
-  }, amount)
-})
+// test('base-observer', function (t) {
+//   const _set = Base.prototype.set
+//   const o = new Base({
+//     define: {
+//       set (val) {
+//         var on = this._on
+//         if (on) {
+//           for (var i = 0, len = on.length; i < len; i++) {
+//             on[i](val)
+//           }
+//         }
+//         return _set.apply(this, arguments)
+//       },
+//       on (fn) {
+//         if (!this._on) {
+//           this._on = []
+//         }
+//         this._on.push(fn)
+//       }
+//     }
+//   })
+//   var cnt = 0
+//   var callCount = 0
+//   o.on(() => ++callCount)
+//   o.on(() => ++callCount)
+//   perf(() => o.set(++cnt), (ms) => {
+//     console.log('vigour-base-observer', ms + 'ms', callCount, Math.round((ms / observResult) * 100) + '%')
+//     t.end()
+//   }, amount)
+// })
 
 test('observable', function (t) {
   const o = new Observable(0)
