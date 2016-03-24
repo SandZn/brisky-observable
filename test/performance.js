b9c7b0425783278083d59048ef91a7915cc3c716
Jim de Beer <jim@vigour.io>
2016-03-24 09:40:42 +0100

    instances, higher peformance, flse stamp in emit

--- test/performance.js
+++ test/performance.js
@@ -69,13 +69,14 @@ test('observable', function (t) {
   var cnt = 0
   o.on(() => ++callCount)
   o.on(() => ++callCount)
-  // const o1 = new o.Constructor()
-  // const o2 = new o.Constructor()
-  // const o3 = new o.Constructor()
+  // can optmize instances lot more pretty sure!
+  const o1 = new o.Constructor()
+  const o2 = new o1.Constructor()
+  const o3 = new o2.Constructor()
   // const o2 = new o1.Constructor()
   // const o3 = new o2.Constructor()
   perf(() => o.set(++cnt), (ms) => {
     console.log('vigour-observable', ms + 'ms', callCount, Math.round((ms / observResult) * 100) + '%')
     t.end()
-  }, Math.round(amount))
+  }, Math.round(amount/4))
 })
