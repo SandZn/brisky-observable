84cdca0ba3aecc5ce2875282d3aca440853f4c92
Jim de Beer <jim@vigour.io>
2016-03-24 22:20:34 +0100

    added fast6 context updates (8 times faster)

--- test/performance.js
+++ test/performance.js
@@ -6,19 +6,10 @@ const Observ = require('observ')
 const test = require('tape')
 const perf = require('vigour-performance').run
 
-var observableResult
-var amount = 1e6
+var observableResult = 283
+var amount = 15e5
 
-// test('base', function (t) {
-//   const o = new Base()
-//   var cnt = 0
-//   // optmize this as well but later
-//   perf(() => o.set(++cnt), (ms) => {
-//     console.log('vigour-base', ms + 'ms')
-//     t.end()
-//   }, amount)
-// })
-test('observable', function (t) {
+test.skip('observable', function (t) {
   const o = new Observable({ key: 'o', val: 0 })
   var callCount = 0
   var cnt = 0
@@ -34,7 +25,7 @@ test('observable', function (t) {
   }, 1)
 })
 
-test('observable-1-level-emit', function (t) {
+test.skip('observable-1-level-emit', function (t) {
   const o = new Observable({ key: 'o', a: 0 })
   var callCount = 0
   var cnt = 0
@@ -49,15 +40,15 @@ test('observable-1-level-emit', function (t) {
   }, 1)
 })
 
-test('observable-2-level-emit', function (t) {
-  // becomes slower because of context
+test.skip('observable-2-level-emit', function (t) {
+  // becomes slower because of context while loops :(
   const o = new Observable({ key: 'o', a: { b: 0 } })
   var callCount = 0
   var cnt = 0
   o.a.b.on(() => ++callCount)
   perf(() => {
     for (var i = 0; i < amount; i++) {
-      o.a.b.set(++cnt)
+      o._a._b.set(++cnt)
     }
   }, (ms) => {
     console.log('vigour-observable', ms + 'ms', callCount, Math.round(ms / observableResult * 100) + '%')
@@ -65,7 +56,28 @@ test('observable-2-level-emit', function (t) {
   }, 1)
 })
 
-test('observ', function (t) {
+test('observable-context', function (t) {
+  const o = new Observable({ key: 'o', a: { b: 0 } })
+  var callCount = 0
+  var cnt = 0
+  o.a.b.on(
+    function () {
+      ++callCount
+    }
+  )
+  const o1 = new o.Constructor({ key: 'oooooo'})
+  perf(() => {
+    // o.a.b.set('hehe')
+    for (var i = 0; i < amount; i++) {
+      o._a._b.set(++cnt)
+    }
+  }, (ms) => {
+    console.log('vigour-observable', ms + 'ms', callCount, Math.round(ms / observableResult * 100) + '%')
+    t.end()
+  }, 1)
+})
+
+test.skip('observ', function (t) {
   const o = Observ(0)
   var callCount = 0
   var cnt = 0
@@ -80,26 +92,7 @@ test('observ', function (t) {
   }, 1)
 })
 
-test('observable-observ (used for element/state)', function (t) {
-  // simmilair construct will be used for state / element
-  var cnt = 0
-  var callCount = 0
-  const o = new Base({
-    set (val, stamp) {
-      ++callCount
-    }
-  })
-  perf(() => {
-    for (var i = 0; i < amount; i++) {
-      o.set(++cnt)
-    }
-  }, (ms) => {
-    console.log('vigour-observable-observ', ms + 'ms', callCount, Math.round((ms / observableResult) * 100) + '%')
-    t.end()
-  }, 1)
-})
-
-test('base-observ', function (t) {
+test.skip('base-observ', function (t) {
   // simmilair construct will be used for state / element
   const o = new Base({
     set (val, stamp) {
@@ -126,6 +119,25 @@ test('base-observ', function (t) {
     }
   }, (ms) => {
     console.log('vigour-base-observ', ms + 'ms', callCount, Math.round((ms / observableResult) * 100) + '%')
+    t.end()
+  }, 1)
+})
+
+test.skip('observable-set-hook (used for element/state)', function (t) {
+  // simmilair construct will be used for state / element
+  var cnt = 0
+  var callCount = 0
+  const o = new Base({
+    set (val, stamp) {
+      ++callCount
+    }
+  })
+  perf(() => {
+    for (var i = 0; i < amount; i++) {
+      o.set(++cnt)
+    }
+  }, (ms) => {
+    console.log('vigour-observable-observ', ms + 'ms', callCount, Math.round((ms / observableResult) * 100) + '%')
     t.end()
   }, 1)
 })
