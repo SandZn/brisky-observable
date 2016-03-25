526197faa58bb54028cf0baad1479684bc646105
Jim de Beer <jim@vigour.io>
2016-03-25 16:49:06 +0100

    instances: false will not track instances for first level if inheritance greatly speeding up creation of, for eample state or observable constructors

--- test/performance.js
+++ test/performance.js
@@ -5,10 +5,11 @@ const Observ = require('observ')
 // const ObservStruct = require('observ-struct')
 const test = require('tape')
 const perf = require('vigour-performance').run
+const vstamp = require('vigour-stamp')
 
 var observableResult = 283
 var amount = 15e5
-amount = 5e6
+// amount = 1e6 // reuslt in 10 mil tests
 // amount = 1
 
 test('observable', function (t) {
@@ -17,17 +18,99 @@ test('observable', function (t) {
   var cnt = 0
   o.on(() => ++callCount)
   perf(() => {
-    for (var i = 0; i < amount * 2; i++) {
+    for (var i = 0; i < amount; i++) {
       o.set(++cnt)
     }
   }, (ms) => {
-    console.log('vigour-observable', ms + 'ms', callCount)
+    console.log('vigour-observable-updates (sets)', ms + 'ms', callCount)
     observableResult = ms
     t.end()
   }, 1)
 })
 
-test.skip('observable-1-level-emit', function (t) {
+test('observable-instances', function (t) {
+  const o = new Observable({ key: 'o', val: 0 })
+  var callCount = 0
+  var cnt = 0
+  var o1 = new o.Constructor()
+  var o2 = new o1.Constructor()
+  var o3 = new o2.Constructor()
+  o.on(() => ++callCount)
+  perf(() => {
+    var a = Math.round(amount/4)
+    for (var i = 0; i < (a); i++) {
+      o.set(++cnt)
+    }
+  }, (ms) => {
+    console.log('vigour-observable-updates (sets)', ms + 'ms', callCount, Math.round(ms / observableResult * 100) + '%')
+    t.end()
+  }, 1)
+})
+
+test('create-normal-object', function (t) {
+  perf(() => {
+    for (var i = 0; i < amount; i++) {
+      var a = { val: i }
+    }
+  }, (ms) => {
+    console.log('create-normal-object', ms + 'ms', Math.round(ms / observableResult * 100) + '%')
+    t.end()
+  })
+})
+
+test('create-base', function (t) {
+  perf(() => {
+    for (var i = 0; i < amount; i++) {
+      var a = new Base(i)
+    }
+  }, (ms) => {
+    console.log('create-base', ms + 'ms', Math.round(ms / observableResult * 100) + '%')
+    t.end()
+  })
+})
+
+test('create-observable (and set using false)', function (t) {
+  perf(() => {
+    for (var i = 0; i < amount; i++) {
+      var a = new Observable(i, false)
+    }
+  }, (ms) => {
+    console.log('create vigour-observable (false)', ms + 'ms', Math.round(ms / observableResult * 100) + '%')
+    t.end()
+  }, 1)
+})
+
+test('create/close stamps', function (t) {
+  perf(() => {
+    var c = vstamp.create
+    var end = vstamp.close
+    for (var i = 0; i < amount; i++) {
+      var s = c()
+      end(s)
+    }
+  }, (ms) => {
+    console.log('vigour-stamp (create/close stamps)', ms + 'ms', Math.round(ms / observableResult * 100) + '%')
+    t.end()
+  }, 1)
+})
+
+test('create-observable (and set creating stamps)', function (t) {
+  perf(() => {
+    // lets optmize this case
+    for (var i = 0; i < amount; i++) {
+      var a = new Observable(i)
+    }
+  }, (ms) => {
+    console.log('vigour-observable (create stamps)', ms + 'ms', Math.round(ms / observableResult * 100) + '%')
+    t.end()
+  }, 1)
+})
+
+// make this all reusable -- after this time for setKey etc
+// then clean up path
+// then do reference
+
+test('observable-1-level-emit', function (t) {
   const o = new Observable({ key: 'o', a: 0 })
   var callCount = 0
   var cnt = 0
@@ -42,7 +125,7 @@ test.skip('observable-1-level-emit', function (t) {
   }, 1)
 })
 
-test.skip('observable-2-level-emit', function (t) {
+test('observable-2-level-emit', function (t) {
   // becomes slower because of context while loops :(
   const o = new Observable({ key: 'o', a: { b: 0 } })
   var callCount = 0
@@ -50,7 +133,7 @@ test.skip('observable-2-level-emit', function (t) {
   o.a.b.on(() => ++callCount)
   perf(() => {
     for (var i = 0; i < amount; i++) {
-      o._a._b.set(++cnt)
+      o.a.b.set(++cnt)
     }
   }, (ms) => {
     console.log('vigour-observable', ms + 'ms', callCount, Math.round(ms / observableResult * 100) + '%')
@@ -58,7 +141,7 @@ test.skip('observable-2-level-emit', function (t) {
   }, 1)
 })
 
-test('observable-context', function (t) {
+test.skip('observable-context', function (t) {
   const o = new Observable({ key: 'o', a: { b: 0 } })
   var callCount = 0
   var cnt = 0
@@ -68,7 +151,7 @@ test('observable-context', function (t) {
       ++callCount
     }
   )
-  const o1 = new o.Constructor({ key: 'oooooo'})
+  const o1 = new o.Constructor({ key: 'oooooo' })
   perf(() => {
     // o.a.b.set('hehe')
     for (var i = 0; i < amount; i++) {
@@ -136,7 +219,7 @@ test('observ', function (t) {
   var cnt = 0
   o(() => ++callCount)
   perf(() => {
-    for (var i = 0; i < amount * 2; i++) {
+    for (var i = 0; i < amount; i++) {
       o.set(++cnt)
     }
   }, (ms) => {
