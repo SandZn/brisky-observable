f63c40da354ac33d0b725b8a9a3e315c6b020ddd
Jim de Beer <jim@vigour.io>
2016-04-01 11:55:36 +0200

    use isObj vs isPlain

--- test/performance/create.js
+++ test/performance/create.js
@@ -1,8 +1,9 @@
-const Observable = require('../../')
 const Base = require('vigour-base')
+const Observable = require('../../')
+
 const test = require('tape')
 const perf = require('vigour-performance').run
-var amount = 1e6
+var amount = 1e7
 var objResult = 0
 var createBase = 0
 
@@ -43,26 +44,26 @@ test('create-base-keys', function (t) {
   })
 })
 
-test('create-observable (and set creating stamps)', function (t) {
-  perf(() => {
-    // lets optmize this case
-    for (var i = 0; i < amount; i++) {
-      var a = new Observable(i)
-    }
-  }, (ms) => {
-    console.log('vigour-observable (create stamps)', ms + 'ms', Math.round(ms / createBase * 100) + '%')
-    t.end()
-  }, 1)
-})
+// test('create-observable (and set creating stamps)', function (t) {
+//   perf(() => {
+//     // lets optmize this case
+//     for (var i = 0; i < amount; i++) {
+//       var a = new Observable(i)
+//     }
+//   }, (ms) => {
+//     console.log('vigour-observable (create stamps)', ms + 'ms', Math.round(ms / createBase * 100) + '%')
+//     t.end()
+//   }, 1)
+// })
 
-test('create-observable-keys', function (t) {
-  perf(() => {
-    var am = Math.round(amount/2)
-    for (var i = 0; i < am; i++) {
-      var a = new Observable({ a: i }, false)
-    }
-  }, (ms) => {
-    console.log('create-observable-keys (no-stamp)', ms + 'ms', Math.round(ms / createBase * 100) + '%')
-    t.end()
-  })
-})
+// test('create-observable-keys', function (t) {
+//   perf(() => {
+//     var am = Math.round(amount/2)
+//     for (var i = 0; i < am; i++) {
+//       var a = new Observable({ a: i }, false)
+//     }
+//   }, (ms) => {
+//     console.log('create-observable-keys (no-stamp)', ms + 'ms', Math.round(ms / createBase * 100) + '%')
+//     t.end()
+//   })
+// })
