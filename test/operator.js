8a27f862b6978d2b90f59ad95470e45988fc8eac
Jim de Beer <jim@vigour.io>
2016-04-05 07:59:11 +0200

    fixing operators

--- test/operator.js
+++ test/operator.js
@@ -1,16 +1,58 @@
 'use strict'
+var vstamp = require('vigour-stamp')
+var c = vstamp.create
+vstamp.create = function () {
+  console.log(new Error('--------').stack)
+  return c.apply(this, arguments)
+}
 var Observable = require('../')
 var test = require('tape')
 
 test('operator', function (t) {
-  console.log('----')
+  console.log('STARTING STAMP:', vstamp.cnt, 'should be 1')
+  var someCondition = false
   var obs = new Observable({
     key: 'obs',
-    $transform () {
-      console.log('TRANSFORM!')
-      return 'flabber'
+    val: 'value',
+    $transform: {
+      val (val) {
+        console.log('transform --->')
+        return val.toUpperCase()
+      },
+      $add () {
+        console.log('add --->')
+        return someCondition
+      }
     }
   })
+
+  console.log('?', obs.$transform._mapProperty)
+  // changing original does not clear the properties of instances thats of course very wrong
+  // this measn we need to track instnces and we dont want that for sure!
+
+  // console.log()
+  console.log('#START OPERATORS -----> _operators in obs.$transform', obs.$transform.keys('_operators'))
   console.log(obs.compute())
+  // t.equal(obs.compute(), 'VALUE', 'transform (string) input value')
+
+  // t.equal(obs.compute(), 'VALUE', 'transform (string) input value')
+
   t.end()
 })
+
+// time operator
+// add / prepend / type
+
+/*
+properties: {
+  $ios: {
+    type: '$prepend', //give it a name add to base
+    $condition {
+      $: '~/client/device', //add to observable
+      $transform (val) {
+        return val === 'ios'
+      }
+    }
+  }
+}
+*/
