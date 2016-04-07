95e70a7a0803760242ec641c17f509965770b40b
Jim de Beer <jim@vigour.io>
2016-04-07 10:49:45 +0200

    correct order of val and stamp in remove emitters

--- test/remove.js
+++ test/remove.js
@@ -1,6 +1,7 @@
 'use strict'
 var Observable = require('../')
 var test = require('tape')
+var vstamp = require('vigour-stamp')
 
 test('remove', function (t) {
   var top = 0
@@ -16,9 +17,13 @@ test('remove', function (t) {
     }
   })
   var instance = new obs.Constructor({ key: 'instance' }, false) // eslint-disable-line
-  t.plan(3)
+  t.plan(5)
   obs.b.remove()
   t.equal(top, 2, 'removed nested field fire for instances')
+  obs.once((val, stamp) => {
+    t.equal(val, null, 'val equals null')
+    t.equal(stamp, vstamp.cnt, 'correct stamp')
+  })
   obs.remove()
   t.equal(top, 4, 'removed nested field fire for instances')
   t.equal(remove, 2, 'removed obs fire for instances')
