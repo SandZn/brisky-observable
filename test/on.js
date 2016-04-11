e3d7807bd9b1baae91c731335f4f68669edf96d6
Jim de Beer <jim@vigour.io>
2016-04-11 14:00:15 +0200

    added tests for failing keys getter (inheritance)

---
+++ test/on.js
@@ -0,0 +1,23 @@
+'use strict'
+const Observable = require('../')
+const Base = require('vigour-base')
+const test = require('tape')
+
+test('on -- has correct keys', function (t) {
+  const Obs = new Observable({
+    Child: 'Constructor'
+  }).Constructor
+  var a = new Obs({
+    b: {
+      c: {
+        on: { hello () {} }
+      }
+    }
+  })
+
+  t.equal(a._on.keys(), false, 'a keys are false')
+  t.equal(a.b._on.keys(), false, 'a.b keys are false')
+  t.deepEqual(a.b.c._on.keys(), [ 'hello' ] , 'a.b.c keys equal [ "hello" ]')
+
+  t.end()
+})
