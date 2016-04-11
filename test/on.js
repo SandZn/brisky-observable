c53d7ef472bfd108ac33c90ffc5f6276da39a697
Jim de Beer <jim@vigour.io>
2016-04-11 14:13:04 +0200

    patched base version (fixes constructor problem)

--- test/on.js
+++ test/on.js
@@ -1,9 +1,33 @@
 'use strict'
-const Observable = require('../')
 const Base = require('vigour-base')
+const Observable = require('../')
 const test = require('tape')
 
-test('on -- has correct keys', function (t) {
+test('base - has correct keys', function (t) {
+  var BaseExample = new Base({
+    something: true,
+    hello: '?'
+  })
+  // BaseExample.set({ Child: 'Constructor' })
+  // BaseExample = BaseExample.Constructor
+
+  console.log('?', BaseExample.something)
+  var a = new BaseExample.Constructor({
+    b: {
+      c: {
+        something: { hello () {} }
+      }
+    }
+  })
+  console.log(BaseExample.Constructor.prototype)
+  console.log(BaseExample.something, a, a instanceof BaseExample.Constructor, a.hello)
+  t.equal(a.something.keys(), false, 'a keys are false')
+  t.equal(a.b.something.keys(), false, 'a.b keys are false')
+  t.deepEqual(a.b.c.something.keys(), [ 'hello' ], 'a.b.c keys equal [ "hello" ]')
+  t.end()
+})
+
+test('on - has correct keys', function (t) {
   const Obs = new Observable({
     Child: 'Constructor'
   }).Constructor
@@ -14,10 +38,8 @@ test('on -- has correct keys', function (t) {
       }
     }
   })
-
   t.equal(a._on.keys(), false, 'a keys are false')
   t.equal(a.b._on.keys(), false, 'a.b keys are false')
-  t.deepEqual(a.b.c._on.keys(), [ 'hello' ] , 'a.b.c keys equal [ "hello" ]')
-
+  t.deepEqual(a.b.c._on.keys(), [ 'hello' ], 'a.b.c keys equal [ "hello" ]')
   t.end()
 })
