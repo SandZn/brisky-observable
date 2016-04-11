e6fe8bcb631219d92a43693616983f4b251fd79c
Jim de Beer <jim@vigour.io>
2016-04-11 14:19:36 +0200

    remove base test

--- test/on.js
+++ test/on.js
@@ -1,31 +1,6 @@
 'use strict'
-const Base = require('vigour-base')
 const Observable = require('../')
 const test = require('tape')
-
-test('base - has correct keys', function (t) {
-  var BaseExample = new Base({
-    something: true,
-    hello: '?'
-  })
-  // BaseExample.set({ Child: 'Constructor' })
-  // BaseExample = BaseExample.Constructor
-
-  console.log('?', BaseExample.something)
-  var a = new BaseExample.Constructor({
-    b: {
-      c: {
-        something: { hello () {} }
-      }
-    }
-  })
-  console.log(BaseExample.Constructor.prototype)
-  console.log(BaseExample.something, a, a instanceof BaseExample.Constructor, a.hello)
-  t.equal(a.something.keys(), false, 'a keys are false')
-  t.equal(a.b.something.keys(), false, 'a.b keys are false')
-  t.deepEqual(a.b.c.something.keys(), [ 'hello' ], 'a.b.c keys equal [ "hello" ]')
-  t.end()
-})
 
 test('on - has correct keys', function (t) {
   const Obs = new Observable({
