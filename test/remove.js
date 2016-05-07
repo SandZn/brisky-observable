301e91c4cbac5022d5143c42c749724cb92b5866
Jim de Beer <jim@vigour.io>
2016-05-07 15:26:56 +0200

    correct clearing of keys when removing

--- test/remove.js
+++ test/remove.js
@@ -7,18 +7,24 @@ test('remove', function (t) {
   var top = 0
   var remove = 0
   var deep = 0
+  var keys
   var obs = new Observable({
     key: 'obs',
     a: { on: { data () { deep++ } } },
     b: true,
     on: {
       remove () { remove++ },
-      data () { top++ }
+      data () {
+        top++
+        keys = this.keys()
+      }
     }
   })
   var instance = new obs.Constructor({ key: 'instance' }, false) // eslint-disable-line
-  t.plan(5)
+  t.plan(6)
+  obs.keys()
   obs.b.remove()
+  t.same(keys, [ 'a' ], 'removed nested field correct keys')
   t.equal(top, 2, 'removed nested field fire for instances')
   obs.once((val, stamp) => {
     t.equal(val, null, 'val equals null')
