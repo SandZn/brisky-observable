a572dd65457365b49d33629f64db0b50570f89be
Jim de Beer <jim@vigour.io>
2016-05-16 09:55:21 +0200

    added standard to tests

--- test/init.js
+++ test/init.js
@@ -4,7 +4,7 @@ const vstamp = require('vigour-stamp')
 
 test('init', function (t) {
   const Observable = require('../')
-  const obs = new Observable({}, false)
+  const obs = new Observable({}, false) // eslint-disable-line
   t.plan(1)
   t.equal(vstamp.cnt, 0, 'vstamp.cnt is zero')
 })
