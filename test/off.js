2fddcb552760f6b31cc7530ee0760e5188893d17
Jim de Beer <jim@vigour.io>
2016-08-18 17:22:50 +0200

    add find by funciton off tests

--- test/off.js
+++ test/off.js
@@ -3,9 +3,23 @@ var Observable = require('../')
 var test = require('tape')
 
 test('off - remove listener by key', function (t) {
-  const obs = new Observable({ on: { data: { a () {} } } })
-  t.same(obs.emitters.data.fn.keys(), [ 'a' ], 'add fn listener a')
+  function labelled () {}
+  const attach = new Observable()
+
+  const obs = new Observable({
+    on: {
+      data: {
+        a () {},
+        b: labelled,
+        c: [ labelled, attach ]
+      }
+    }
+  })
+  t.same(obs.emitters.data.fn.keys(), [ 'a', 'b' ], 'add fn listener a, b')
+  t.same(obs.emitters.data.attach.keys(), [ 'c' ], 'add attach listener c')
   obs.off('data', 'a')
+  obs.off('data', labelled)
+  t.same(obs.emitters.data.attach.keys(), [], 'remove attach listener c')
   t.same(obs.emitters.data.fn.keys(), [], 'remove fn listener a')
   t.end()
 })
