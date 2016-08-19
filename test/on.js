d5d109bc738944eeb2194bde5552b610b0e2c0e9
Jim de Beer <jim@vigour.io>
2016-08-19 11:23:51 +0200

    add test for remove

--- test/on.js
+++ test/on.js
@@ -62,13 +62,14 @@ test('on - resolve context (set)', (t) => {
   t.end()
 })
 
-// do a rewrite of the dirty dirty on/off syntax
-// its so mess it not even funny
-
-// OVERWRITE TESTS √
-// also need to perf test that shit
-
-// RESOLVE TESTS <--- get this shit in here
-
-//   remove.js         |    83.87 |       76 |      100 |    83.64 |... 56,76,77,78 | needs way more tests
-//   storage.js        |    96.43 |    61.54 |      100 |    96.43 |             49 |
+test('on - resolve context remove (set)', (t) => {
+  const obs = new Observable({ a: { on: { data () {} } } })
+  const instance = new obs.Constructor({ key: 'instance' })
+  t.same(obs.a.emitters.data.fn.keys(), [ 'val' ], 'data.fn has val')
+  instance.set({ a: { on: { data: null } } })
+  t.same(obs.a.emitters.data.fn.keys(), [ 'val' ], 'data.fn does not get extra listeners')
+  t.same(instance.a._emitters.data, null, 'instance does not have data emitter')
+  t.ok(instance.a.hasOwnProperty('_emitters'), 'instance has own emitters')
+  t.ok(instance.a._emitters.hasOwnProperty('_data'), 'emitters own data property')
+  t.end()
+})
